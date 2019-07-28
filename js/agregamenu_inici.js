8f0b5738e527c2e17e63ddd4de539940edfbaa6f
admin <admin@192.168.1.17>
2019-07-28 20:52:17 +0200

    index

--- js/agregamenu_inici.js
+++ js/agregamenu_inici.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
      
        $( "#menu_inici" ).load( "menu_inici.html" );
-       $('#fotos').append('<img class="img-fluid" src="img/index1.jpg">');
+       
  });
 
